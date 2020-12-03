b021d75cbb3738b3d5c7b38a92985e7f2d691f94
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2020-12-03 08:25:28 -0600

    add initial sort function

--- selection_sort.py
+++ selection_sort.py
@@ -1,2 +1,10 @@
-numbers = [4,1,6,2,5,8,7,9]
+numbers = [4,1,6,2,51,8,14,9]
 
+# takes list, compares index with next index, moves index if index +1 is lower, return list
+def num_sort(list):
+    for i in range(len(list)-1):
+        if list[i] > list[i+1]:
+            list[i], list[i+1] = list[i+1], list[i]
+    return list
+
+print(num_sort(numbers))
