8df7bbc2f1b406afee49ec68c4db2f70a65b1001
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2020-12-03 19:22:16 -0600

    add basic problem 6

--- basic_13.py
+++ basic_13.py
@@ -31,10 +31,25 @@
 # print(odd())
 
 # 5 
-def greater(list, y):
-    count = 0
+# def greater(list, y):
+#     count = 0
+#     for x in range(len(list)):
+#         if list[x] > y:
+#             count += 1
+#     return count
+# print(greater([1,2,31,45,2,61,17], 4))
+
+# 6
+def min_max_avg(list):
+    min = list[0]
+    max = list[0]
+    sum = 0
     for x in range(len(list)):
-        if list[x] > y:
-            count += 1
-    return count
-print(greater([1,2,31,45,2,61,17], 4))
+        if list[x] < min:
+            min = list[x]
+        if list[x] > max:
+            max = list[x]
+        sum += list[x]
+    avg = sum/len(list)
+    return min, max, avg
+print(min_max_avg([1,3,2,12,16, -2, 13]))
