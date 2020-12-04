2218a21260a3f85975a40677d53e53053b2faa9e
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2020-12-03 19:09:24 -0600

    add basic problem 2

--- basic_13.py
+++ basic_13.py
@@ -1,5 +1,13 @@
 # 1
-def print_basic():
-    for x in range(1,256):
-        print(x)
-print_basic()
+# def print_basic():
+#     for x in range(1,256):
+#         print(x)
+# print_basic()
+
+# 2
+def print_sum():
+    sum = 0
+    for x in range(1, 256):
+        sum += 1
+    print(sum)
+print_sum()
