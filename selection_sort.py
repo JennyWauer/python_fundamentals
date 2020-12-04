4db3641435d2d724242152b1697a8f0bc1d7f0d0
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2020-12-03 18:59:09 -0600

    update sort function

--- selection_sort.py
+++ selection_sort.py
@@ -2,9 +2,13 @@ numbers = [4,1,6,2,51,8,14,9]
 
 # takes list, compares index with next index, moves index if index +1 is lower, return list
 def num_sort(list):
-    for i in range(len(list)-1):
-        if list[i] > list[i+1]:
-            list[i], list[i+1] = list[i+1], list[i]
+    min = list[0]
+    for i in range(len(list)):
+        if list[i]<min:
+            min = list[i]
+            
+        print(min)
+           
     return list
 
 print(num_sort(numbers))
