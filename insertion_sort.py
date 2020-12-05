69ac72fe490bb61ee6d09aa34cdae8fc81ec1224
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2020-12-05 09:10:49 -0600

    add pseudocode

---
+++ insertion_sort.py
@@ -0,0 +1,5 @@
+# Take array
+# compare values to the previous values in the list
+# move the smalest value to a temp value
+# shift bigger values to the right
+# loop through this for each index in the list
