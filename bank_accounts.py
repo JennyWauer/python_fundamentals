d1cacea55f5ac0a315645190231e5e5184642d7a
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2020-12-10 18:47:19 -0600

    add functionality

--- bank_accounts.py
+++ bank_accounts.py
@@ -21,6 +21,32 @@ class User:		# here's what we have so far
         other_user.make_deposit(amount)
         return self
 
+class BankAccount:
+    def __init__ (self, int_rate, balance):
+        self.int_rate = int_rate
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+    
+    def withdrawl(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+        else:
+            print('Insufficient funts: Charging a $5 fee')
+            self.balance -= 5
+        return self
+    
+    def display_account_info(self):
+        print('Balance: ${}'.format(self.balance))
+        return self
+    
+    def yield_interest(self):
+        if self.balance >= 0:
+            self.balance += (self.balance * self.int_rate)
+        return self
+
 jenny = User('Jenny', 'test@test.com')
 
 zach = User('Zach', 'test2@test.com')
@@ -32,3 +58,11 @@ jenny.make_deposit(5000).make_deposit(150).make_deposit(1000).make_withdrawl(100
 zach.make_deposit(2000).make_deposit(1000).make_withdrawl(100).make_withdrawl(3000).display_user_balance()
 
 sam.make_deposit(3000).make_withdrawl(1000).make_withdrawl(1000).make_withdrawl(150).display_user_balance()
+
+account1 = BankAccount(.05, 200)
+
+account2 = BankAccount(.1, 2000)
+
+account1.deposit(1000).deposit(150).deposit(250).withdrawl(200).yield_interest().display_account_info()
+
+account2.deposit(1000).deposit(3000).withdrawl(2000).withdrawl(20).withdrawl(1000).withdrawl(55).yield_interest().display_account_info()
