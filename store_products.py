7deaedb20a6d88388831cb2ae50486557633d363
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2020-12-12 15:48:07 -0600

    add product class

--- store_products.py
+++ store_products.py
@@ -3,3 +3,8 @@ class Store:
         self.name = name
         self.product_list = []
 
+class Product:
+    def __init__(self, name, price, category):
+        self.name = name
+        self.price = price
+        self.category = category
