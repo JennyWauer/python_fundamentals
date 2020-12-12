53a055d7464021cec2cae45f76cecbe2076d9aaa
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2020-12-12 15:26:51 -0600

    added test code

--- modularizing/parent.py
+++ modularizing/parent.py
@@ -6,3 +6,9 @@ class User:
         self.name = name
     def say_hello(self):
         return "hello"
+
+# in the same file, add the following below the User class
+print(square(5))
+user = User("Anna")
+print(user.name)
+print(user.say_hello())
